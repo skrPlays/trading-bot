a714414d7d31d15aae1f81830b339eebdcb30933
jaisaxena <jai.saxena.1028@gmail.com>
2021-05-30 16:55:43 +0530

    Basic Structural changes and Modules
    made basic str changes to the project and added 2 files. Use v3 to differentiate from the original bot, will refactor everything later

---
+++ v3_ConstantLib.py
@@ -0,0 +1,5 @@
+
+HOME = "C:\\Users\\jai saxena\\PycharmProjects\\TradingBot\\"
+LOGFILE = "C:\\Users\\jai saxena\\PycharmProjects\\TradingBot\\logfile.log"
+
+SAMPLE_HISTORICAL = "C:\\Users\\jai saxena\\PycharmProjects\\TradingBot\\SampleData\\historicaldata.txt"
