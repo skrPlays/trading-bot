6270cbda28e004a20c9c68d1a84aeeaf3ec893c9
jaisaxena <jai.saxena.1028@gmail.com>
2021-05-30 23:19:22 +0530

    Revert "Basic Structural changes and Modules"
    This reverts commit a714414d7d31d15aae1f81830b339eebdcb30933.

--- v3_ConstantLib.py
+++
@@ -1,5 +0,0 @@
-
-HOME = "C:\\Users\\jai saxena\\PycharmProjects\\TradingBot\\"
-LOGFILE = "C:\\Users\\jai saxena\\PycharmProjects\\TradingBot\\logfile.log"
-
-SAMPLE_HISTORICAL = "C:\\Users\\jai saxena\\PycharmProjects\\TradingBot\\SampleData\\historicaldata.txt"
